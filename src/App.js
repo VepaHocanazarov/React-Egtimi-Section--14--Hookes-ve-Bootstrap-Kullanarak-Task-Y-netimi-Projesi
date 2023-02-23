bf1584e8e2487f5e64abceb889ac8197ca46ea34
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-23 11:59:31 +0300

    146. Task Ekleme Fonksiyonu

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import CreateTask from './CreateTask';
 import Header from './Header';
 
 function App() {
+
+ const onTask = (newTask)=>{
+    console.log(newTask)
+  }
   return (
     <div className="App">
       <Header/>
-      <CreateTask/>
+      <CreateTask onAdd = {onTask}/>
     </div>
   );
 }
--- src/CreateTask.jsx
+++ src/CreateTask.jsx
@@ -1,22 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function CreateTask() {
+export default function CreateTask(props) {
+
+  const [task,setTask] = useState({
+    title:"",
+    content:"",
+  });
+
+  function detectTask(event){
+    // console.log(event.target.value);
+      
+    const {name,value} = event.target;
+
+    setTask((prevTask)=>{
+      return{
+        ...prevTask,
+        [name]:value,
+      }
+    })
+  };
+
+
+  function submitTask (event){
+    props.onAdd(task);
+    setTask({
+      title:"",
+      content:"",
+    })
+    event.preventDefault();
+  }
+
+
+
   return (
     <div className='todoDivArea'>
         <form className='todoDiv' >
           <input type="text"
           className='form-control todoText mb-3'
           name='title'
+          onChange={detectTask}
+          value={task.title}
           placeholder='Başlık'/>
 
           <textarea 
           name="content" 
+          value={task.content}
+          onChange={detectTask}
           className='form-control todoText mb-3'
            rows={3}
            placeholder='İşinizi yazınız'
            ></textarea>
 
-           <button className='btn btn-primary  todoButton'>Ekle</button>
+           <button className='btn btn-primary  todoButton' onClick={submitTask} >Ekle</button>
         </form>
     </div>
   )
