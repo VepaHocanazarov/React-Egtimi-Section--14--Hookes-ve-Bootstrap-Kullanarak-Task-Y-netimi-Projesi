5281fc5c931c15c96733cb9083e43bbb1aef2b51
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-23 14:25:44 +0300

    147. Listeleme ve Silme Aksiyonu

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
+import { useState } from 'react';
 import './App.css';
 import CreateTask from './CreateTask';
 import Header from './Header';
+import Task from "./Task"
 
 function App() {
 
- const onTask = (newTask)=>{
-    console.log(newTask)
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (newTask) => {
+    setTasks((prevTask) => {
+      return [...prevTask, newTask];
+    })
+  };
+
+  const deleteTask = (id) => {
+    setTasks((prevTask) => {
+      return prevTask.filter((taskItem, index) => {
+        return index !== id;
+      })
+    })
   }
   return (
     <div className="App">
-      <Header/>
-      <CreateTask onAdd = {onTask}/>
+      <Header />
+      <CreateTask onAdd={addTask} />
+      <div className='container'>
+        <div className="row my-5">
+          {tasks.map((taskItem, index) => {
+            return (
+              <Task
+                key={index}
+                id={index}
+                title={taskItem.title}
+                content={taskItem.content}
+                delete={deleteTask}
+              />
+            )
+          })}
+        </div>
+      </div>
+
+
     </div>
   );
 }
